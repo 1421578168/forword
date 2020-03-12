e156977bd56bd4c35265720bb8fb0c7f4d218f71
1421578168 <1421578168@qq.com>
2020-03-12 18:47:16 +0800

    首次提交

--- src/com/dong/filter/EncodingFilter.java
+++ src/com/dong/filter/EncodingFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "EncodingFilter",urlPatterns = "/houtai/*")
+@WebFilter(filterName = "EncodingFilter",urlPatterns = "/qiantai/*")
 public class EncodingFilter implements Filter {
     public void destroy() {
     }
